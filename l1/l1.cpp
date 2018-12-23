17f8bb78919b8843e766a78997abe16941330a19
Yamakawa Makoto <mako.yamakawa@gmail.com>
2018-12-23 21:28:11 +0900

    lost1

--- l1/l1.cpp
+++ l1/l1.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
+int sum(int a, int b);
 
 int main()
 {
+	int a = 1, b = 10, n;
+
+	n = sum(a, b);
+	cout << "‘˜a=" << n << '\n';
+
     return 0;
 }
 
+int sum(int x, int y) {
+	int i, tmp = 0;
+	for (i = x; i <= y; i++) {
+		tmp += i;
+	}
+	return tmp;
+}
